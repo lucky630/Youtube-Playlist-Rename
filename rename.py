97e8b1bc5c56ad58680787165745fdeaf0655acc
rednivrug <gurvinder630@yahoo.com>
2017-11-18 02:12:35 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -29,7 +29,7 @@ def natural_keys(text):
 	return [ atoi(c) for c in re.split('(\d+)', text) ]
 
 b1.sort(key=natural_keys)		#this one sort the video name having(string_number)
-##this will loop through the code and rename the files.
+##this will loop through the dictionary and rename the files.
 for i in b1:
     nm=i.split('_')[1].split('.')[0]
     if nm in dd1.keys():
